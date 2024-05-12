6c14b991a25ac3bcba8fc8b1184c98279744c438
tehnickge <71207932+tehnickge@users.noreply.github.com>
2024-05-12 23:10:03 +0300

    add calc and binds

--- simpleApp/src/app/calculator/calculator.component.ts
+++ simpleApp/src/app/calculator/calculator.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
+  public first: number = 1;
+  public second: number = 1;  
 
+  public operation: string = "+";
+  public operations: string[] = ["+", "-", "/", "*"];
+
+  public result?: number = undefined;
+
+  public calc() {
+    switch(this.operation) {
+      case '+':
+        this.result = this.first + this.second;
+        break;
+        case '-':
+        this.result = this.first - this.second;
+        break;
+        case '*':
+        this.result = this.first * this.second;
+        break;
+        case '/':
+        this.result = this.first / this.second;
+        break;
+    }
+  }
 }
