49b6e7748c599388e14fbca5de99a6094bede571
tehnickge <71207932+tehnickge@users.noreply.github.com>
2024-05-15 21:39:39 +0300

    add routing

--- simpleApp/src/app/app.routes.ts
+++ simpleApp/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { CalculatorModule } from './calculator/calculator.module';
+import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/components/calculator.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        title: "calculator",
+        path: "calculator",
+        component: CalculatorComponent
+    },
+    {
+        title: "dom",
+        path: "",
+        component: AppComponent
+    },
+    {
+        title: "dom",
+        path: "**",
+        component: ErrorPageComponent
+    }
+];
