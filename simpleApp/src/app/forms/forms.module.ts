702a5829d9eaec2869c77ae6f5913a0c28af5c8b
Nikita "Tehnick <71207932+tehnickge@users.noreply.github.com>
2024-07-24 00:36:11 +0300

    add formm and any

---
+++ simpleApp/src/app/forms/forms.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsComponent } from './components/forms/forms.component';
+import { FormsRouting } from './forms-routing.module';
+//import { RateComponent } from './components/rate/rate.component';
+
+@NgModule({
+  declarations: [
+    FormsComponent,
+    //   RateComponent
+  ],
+  imports: [CommonModule, FormsRouting, ReactiveFormsModule, FormsModule],
+})
+export class FormsUIModule {}
